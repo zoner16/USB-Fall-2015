edab527369fcda49eeec9783346df60c479ef823
Suman Amugothu <samugoth@ece-gui-001.ece.cmu.edu>
2015-11-03 11:37:44 -0500

    completed first draft of rwFSM and pFSM

---
+++ protocolFSM.sv
@@ -0,0 +1,113 @@
+module protocolFSM
+  (input bit clk, rst_b,
+   input bit in_trans, out_trans,
+   input bit data_ready,
+   input bit [63:0] data_from_device, data_from_host,
+   input bit crc_correct,
+   input bit [3:0] pid_in;
+   output bit [3:0] pid, endp,
+   output bit [6:0] addr,
+   output bit failure, success,
+   output bit [4:0] crc_type,
+   output bit [63:0] data_to_host, data_to_device);
+
+   logic [7:0] 	    clk_count;
+   logic [3:0] 	    timeout_count;
+   logic [3:0] 	    corrupted_count;   
+   
+   enum  logic {Hold= 2'd0, InTrans= 2'd1, OutTransData= 2'd2, OutTrans= 2'd3} state;
+   
+   always_ff @(posedge clk, negedge rst_b) begin
+      if (~rst_b) begin
+	 state <= Hold;
+	 timeout_count <= 0;
+	 clk_count <= 0;
+	 failure <= 0;
+	 success <= 0;
+	 corrupted_count <= 0;
+      end
+      else begin
+	 case(state)
+	   Hold: begin
+	      clk_count <= 0;
+	      failure <= 0;
+	      success <= 0;
+	      timeout_count <= 0;
+	      corrupted_count <= 0;
+	      if (in_trans) begin
+		 pid <= 4'b1001;
+		 addr <= 7'd5;
+		 endp <= 4'd4;
+		 crc_type <= 5'd5;
+		 state <= InTrans;
+	      end
+	      else if (out_trans) begin
+		 pid <= 4'b0001;
+		 addr <= 7'd5;
+		 endp <= 4'd4;
+		 crc_type <= 5'd5; 
+		 state <= OutTransData;
+	      end
+	   end
+	   InTrans: begin
+	      if (corrupted_count == 4'd8) begin // corrupted max reached
+		 state <= Hold;
+		 failure <= 1;
+	      end
+	      else if (timeout_count == 4'd8) begin // timeout max reached
+		 state <= Hold;
+		 failure <= 1;
+	      end
+	      else if (data_ready && ~crc_correct) begin // corrupted data sent back
+		 corrupted_count <= corrupted_count + 1;
+		 clk_count <= 0;
+		 pid <= 4'b1010; //send nak
+	      end
+	      else if (data_ready && crc_correct) begin //correct data sent back
+		 state <= Hold;
+		 success <= 1;
+		 pid <= 4'b0010; //send ack
+		 data_to_host <= data_from_device;
+	      end 
+	      else if (clk_count == 8'd255) begin //timeout reached
+		 timeout_count <= timeout_count + 1'd1;
+		 clk_count <= 0;
+	      end
+	      else 
+		clk_count <= clk_count + 1'd1; //keep track of clk cycles
+	   end // case: InTrans
+	   OutTransData: begin // send data
+	      pid <= 4'b0011;
+	      data_to_device <= data_from_host;
+	      crc_type = 5'd16;
+	      state <= OutTrans;
+	   end
+	   OutTrans: begin
+	      if (corrupted_count == 4'd8) begin // corrupted max reached
+		 state <= Hold;
+		 failure <= 1;
+	      end
+	      else if (timeout_count == 4'd8) begin // timeout max reached
+		 state <= Hold;
+		 failure <= 1;
+	      end
+	      else if (pid_in == 4'b0010) begin
+		 state <= OutTransData; // received nak so resend data
+		 corrupted_count <= corrupted_count + 1'd1;
+	      end
+	      else if (pid_in == 4'b0010) begin
+		 state <= Hold;
+		 success <= 1;
+	      end
+	      else if (clk_count == 8'd255) begin //timeout reached
+		 timeout_count <= timeout_count + 1'd1;
+		 clk_count <= 0;
+	      end
+	      else 
+		clk_count <= clk_count + 1'd1; //keep track of clk cycles
+	   end
+	 endcase
+      end
+   end
+
+endmodule: protocolFSM
--- readWriteFSM.sv
+++ readWriteFSM.sv
@@ -1,28 +1,76 @@
 module readWriteFSM
- (input bit clk, rst_b,
-  input bit [15:0] FSMmempage,
-  input bit [63:0] data_in,
-  input bit read,
-  input bit write,
-  output bit [63:0] valueRead,
-  output bit done);
-
-
-	always_ff @(posedge clk, negedge rst_b) begin
-		if (~rst_b) state <= Hold;
-		else begin
-			case(state)
-                Hold: 
-                     if (read) state <= Read;
-                     else if (write) state <= Write;
-                ReadOut: // out trans then in
-                    outTrans <= 1;
-                    protocolMemPage <= FSMmempage;
-                    if (readOutDone) state <= ReadIn;
-                ReadIn:
-                Write: // out trans then out
-		    endcase
-	end
-  end 
+  (input bit clk, rst_b,
+   input bit [15:0] FSMmempage,
+   input bit [63:0] data_from_OS, data_from_device,
+   input bit read,
+   input bit write,
+   output bit [63:0] data_to_device, data_to_host,
+   output bit in_trans, out_trans,
+   output bit isValueReadCorrect,
+   output bit read_write_FSM_done);
+   
+   
+   always_ff @(posedge clk, negedge rst_b) begin
+      if (~rst_b) begin
+	 state <= Hold;
+	 isValueReadCorrect <= 0;
+	 read_write_FSM_done <= 0;
+	 in_trans <= 0;
+	 out_trans <= 0;
+      end
+      else begin
+	 case(state)
+           Hold: begin
+	      isValueReadCorrect <= 0;
+	      read_write_FSM_done <= 0;
+	      in_trans <= 0;
+	      out_trans <= 0;
+              if (read || write) state <= ReadOut;
+	   end
+           Out: begin // out trans then in
+              out_trans <= 1;
+              data_to_device <= {48'd0,FSMmempage}; //addr gets sent out as data
+	      if (success) begin
+		 state <= ReadIn;
+	      end
+	      else if (failure) begin
+		 state <= Hold;
+		 read_write_FSM_done <= 1;
+		 isValueReadCorrect <= 0;
+	      end
+	      if (read) state <= ReadIn;
+	      else if (write) state <= WriteOut;
+	   end
+           ReadIn: begin
+	      in_trans <= 1;
+	      data_to_OS <= data_from_device;
+	      if (success) begin
+		 state <= Hold;
+		 read_write_FSM_done <= 1;
+		 isValueReadCorrect <= 1;
+	      end
+	      else if (failure) begin
+		 state <= Hold;
+		 read_write_FSM_done <= 1;
+		 isValueReadCorrect <= 0;
+	      end
+	   end // case: ReadIn
+	   WriteOut: begin
+	      out_trans <= 1;
+	      data_to_device <= data_from_OS;
+	      if (success) begin
+		 state <= Hold;
+		 read_write_FSM_done <= 1;
+		 isValueReadCorrect <= 1;
+	      end
+	      else if (failure) begin
+		 state <= Hold;
+		 read_write_FSM_done <= 1;
+		 isValueReadCorrect <= 0;
+	      end
+	   end
+	 endcase
+      end
+   end 
 
 endmodule: readWriteFSM

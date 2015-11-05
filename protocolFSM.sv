165886f73638b732220df116acda17aaa61411bb
Brad Powell <bradpowell730@,gmail.com>
2015-11-04 21:13:46 -0500

    Reformatted FSMs

--- protocolFSM.sv
+++ protocolFSM.sv
@@ -1,24 +1,23 @@
 module protocolFSM
-  (input bit clk, rst_b, in_trans, out_trans, pkt_sent, pkt_received, crc_correct, encode, decode,
-   input 	     pkt_t pkt_in,
+  (input bit         clk, rst_b, in_trans, out_trans, pkt_sent, pkt_received, crc_correct, encode, decode,
+   input pkt_t       pkt_in,
    input bit [63:0]  data_from_host,
    output bit 	     failure, success, kill,
-   output 	     pkt_t pkt_out,
+   output pkt_t      pkt_out,
    output bit [4:0]  crc_type,
    output bit [63:0] data_to_host);
 
    logic [7:0] 	    clk_count;
    logic [3:0] 	    timeout_count, corrupted_count;   
    
-   enum logic [2:0] {Hold= 3'd0, InTransWait= 3'd1, InTrans = 3'd2,
-		     OutTransWait = 2'd3, OutTransDataWait= 3'd4, OutTrans= 3'd5} state;
+   enum logic [2:0] {Hold= 3'd0, InTransWait= 3'd1, InTrans = 3'd2, OutTransWait = 2'd3, OutTransDataWait= 3'd4, OutTrans= 3'd5} state;
    
     always_ff @(posedge clk, negedge rst_b) begin
         if (~rst_b) begin
             state <= Hold;
-	    encode <= 0;
-	    decode <= 0;
-	    kill <= 0;
+            encode <= 0;
+            decode <= 0;
+            kill <= 0;
             timeout_count <= 0;
             clk_count <= 0;
             failure <= 0;
@@ -38,8 +37,8 @@ module protocolFSM
                         pkt_out.addr <= 7'd5;
                         pkt_out.endp <= 4'd4;
                         crc_type <= 5'd5;
-		        encode <= 1;
-		        kill <= 1;
+                        encode <= 1;
+                        kill <= 1;
                         state <= InTransWait;
                     end
                     else if (out_trans) begin
@@ -47,22 +46,22 @@ module protocolFSM
                         pkt_out.addr <= 7'd5;
                         pkt_out.endp <= 4'd4;
                         crc_type <= 5'd5;
-		        encode <= 1;
-		        kill <= 1;
+                        encode <= 1;
+                        kill <= 1;
                         state <= OutTransWait;
                     end
                 end // case: Hold
-	        InTransWait: begin // sent a packet and waiting for the signal that it was sent
-		     clk_count <= 0;
-		     encode <= 0;
-		     kill <= 0;
-		     if (pkt_sent) begin //datastream_out confirmed pckt sent
-		        state <= InTrans;
-		        decode <= 1;
-		     end
-		 end
+                InTransWait: begin // sent a packet and waiting for the signal that it was sent
+                    clk_count <= 0;
+                    encode <= 0;
+                    kill <= 0;
+                    if (pkt_sent) begin //datastream_out confirmed pckt sent
+                        state <= InTrans;
+                        decode <= 1;
+                    end
+                end
                 InTrans: begin
-		   decode <= 0;
+                    decode <= 0;
                     if (corrupted_count == 4'd8) begin // corrupted max reached
                         state <= Hold;
                         failure <= 1;
@@ -74,52 +73,52 @@ module protocolFSM
                     else if (pkt_received && ~crc_correct) begin // corrupted data sent back
                         corrupted_count <= corrupted_count + 1;
                         pkt_out.pid <= 4'b1010; //send nak
-		        encode <= 1;
-		        kill <= 1;
-		        state <= InTransWait;
+                        encode <= 1;
+                        kill <= 1;
+                        state <= InTransWait;
                     end
                     else if (pkt_received && crc_correct) begin //correct data sent back
                         state <= Hold;
                         success <= 1;
                         pkt_out.pid <= 4'b0010; //send ack
-		        encode <= 1;
-		        kill <= 1;
+                        encode <= 1;
+                        kill <= 1;
                         data_to_host <= pkt_in.data;
                     end 
                     else if (clk_count == 8'd255) begin //timeout reached
                         timeout_count <= timeout_count + 1'd1;
-		        pkt_out.pid <= 4'b1010; //send nak
-		        encode <= 1;
-		        kill <= 1;
-		        state <= InTransWait;
+                        pkt_out.pid <= 4'b1010; //send nak
+                        encode <= 1;
+                        kill <= 1;
+                        state <= InTransWait;
                     end
                     else begin
                         clk_count <= clk_count + 1'd1; //keep track of clk cycles
                     end
                 end // case: InTrans
-	        OutTransWait: begin //wait for datastream to confirm out pkt sent
-		   encode <= 0;
-		   kill <= 0;
-		   if (pkt_sent) begin
-		      state <= OutTransDataWait;
-		      pkt_out.pid <= 4'b0011; //send data
-                      pkt_out.data <= data_from_host;
-                      crc_type = 5'd16;
-		      encode <= 1;
-		      kill <= 1;
-		   end
-		end
+                OutTransWait: begin //wait for datastream to confirm out pkt sent
+                    encode <= 0;
+                    kill <= 0;
+                    if (pkt_sent) begin
+                        state <= OutTransDataWait;
+                        pkt_out.pid <= 4'b0011; //send data
+                        pkt_out.data <= data_from_host;
+                        crc_type = 5'd16;
+                        encode <= 1;
+                        kill <= 1;
+                    end
+                end
                 OutTransDataWait: begin //wait for datastream to confirm data pkt sent
-		   clk_count <= 0;
-                   encode <= 0;
-		   kill <= 0;
-		   if (pkt_sent) begin
-		      state <= OutTrans;
-		      decode <= 1;
-		   end
+                    clk_count <= 0;
+                    encode <= 0;
+                    kill <= 0;
+                    if (pkt_sent) begin
+                        state <= OutTrans;
+                        decode <= 1;
+                    end
                 end
                 OutTrans: begin
-		    decode <= 0;
+                    decode <= 0;
                     if (corrupted_count == 4'd8) begin // corrupted max reached
                         state <= Hold;
                         failure <= 1;
@@ -130,13 +129,13 @@ module protocolFSM
                     end
                     else if (pkt_received && pkt_in.pid == 4'b1010) begin
                          // received nak so resend data
-		       state <= OutTransDataWait;
-		       pkt_out.pid <= 4'b0011;
-                       pkt_out.data <= data_from_host;
-                       crc_type = 5'd16;
-		       encode <= 1;
-		       kill <= 1;
-                       corrupted_count <= corrupted_count + 1'd1;
+                        state <= OutTransDataWait;
+                        pkt_out.pid <= 4'b0011;
+                        pkt_out.data <= data_from_host;
+                        crc_type = 5'd16;
+                        encode <= 1;
+                        kill <= 1;
+                        corrupted_count <= corrupted_count + 1'd1;
                     end
                     else if (pkt_received && pkt_in.pid == 4'b0010) begin //received ack
                         state <= Hold;
@@ -144,12 +143,12 @@ module protocolFSM
                     end
                     else if (clk_count == 8'd255) begin //timeout reached so resend data
                         timeout_count <= timeout_count + 1'd1;
-		        state <= OutTransDataWait;
-		        pkt_out.pid <= 4'b0011;
+                        state <= OutTransDataWait;
+                        pkt_out.pid <= 4'b0011;
                         pkt_out.data <= data_from_host;
                         crc_type = 5'd16;
-		        encode <= 1;
-		        kill <= 1;
+                        encode <= 1;
+                        kill <= 1;
                     end
                     else begin
                         clk_count <= clk_count + 1'd1; //keep track of clk cycles
--- readWriteFSM.sv
+++ readWriteFSM.sv
@@ -1,9 +1,9 @@
 module readWriteFSM
-  (input bit clk, rst_b, read, write, failure, success,
-   input bit [15:0] FSMmempage,
-   input bit [63:0] data_from_OS, data_from_device,
+  (input bit         clk, rst_b, read, write, failure, success,
+   input bit [15:0]  FSMmempage,
+   input bit [63:0]  data_from_OS, data_from_device,
    output bit [63:0] data_to_device, data_to_OS,
-   output bit in_trans, out_trans, isValueReadCorrect, read_write_FSM_done);
+   output bit        in_trans, out_trans, isValueReadCorrect, read_write_FSM_done);
    
     enum logic [1:0] {Hold = 2'b00, Out = 2'b01, ReadIn = 2'b10, WriteOut = 2'b11} state;
    
@@ -24,23 +24,23 @@ module readWriteFSM
                     in_trans <= 0;
                     out_trans <= 0;
                     if (read || write) begin
-                       state <= Out;
-		       out_trans <= 1;
-                       data_to_device <= {48'd0, FSMmempage};
+                        state <= Out;
+                        out_trans <= 1;
+                        data_to_device <= {48'd0, FSMmempage};
                     end
                 end
                 Out: begin // out trans then in
-		    out_trans <= 0;
+                out_trans <= 0;
                     if (success) begin
-		       if (read) begin
-			  state <= ReadIn;
-			  in_trans <= 1;
-		       end
-		       else begin
-			  state <= WriteOut;
-			  out_trans <= 1;
-			  data_to_device <= data_from_OS;
-		       end
+                        if (read) begin
+                            state <= ReadIn;
+                            in_trans <= 1;
+                        end
+                        else begin
+                            state <= WriteOut;
+                            out_trans <= 1;
+                            data_to_device <= data_from_OS;
+                        end
                     end
                     else if (failure) begin
                         state <= Hold;
@@ -50,7 +50,7 @@ module readWriteFSM
                 end
                 ReadIn: begin
                     in_trans <= 0;
-		    data_to_OS <= data_from_device;
+                    data_to_OS <= data_from_device;
                     if (success) begin
                         state <= Hold;
                         read_write_FSM_done <= 1;
